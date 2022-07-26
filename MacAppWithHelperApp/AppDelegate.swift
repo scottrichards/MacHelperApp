127f459ca45755999777c97e12fd477462cc782b
scottrichards <scott.srichards@gmail.com>
2022-07-26 14:23:00 +0200

    Got a Modal Window to Show Up

--- MacAppWithHelperApp/AppDelegate.swift
+++ MacAppWithHelperApp/AppDelegate.swift
@@ -12,6 +12,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     private var window: NSWindow!
     private var statusItem: NSStatusItem!
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
@@ -24,20 +26,55 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 //        window.title = "No Storyboard Window"
 //        window.makeKeyAndOrderFront(nil)
         // 2
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+         
+        // Create the popover
+       let popover = NSPopover()
+       popover.contentSize = NSSize(width: 400, height: 500)
+       popover.behavior = .transient
+//       popover.contentViewController = NSHostingController(rootView: contentView)
+       self.popover = popover
+        
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         // 3
-        if let button = statusItem.button {
+        if let button = statusBarItem.button {
             button.image = NSImage(systemSymbolName: "checkmark.circle", accessibilityDescription: "1")
             button.target = self
-            button.action = #selector(AppDelegate.showHelperApp)
+            button.action = #selector(showHelperApp)
         }
     }
     
     
     @objc func showHelperApp() {
-        print("Show Helper App")
+        print("Openeing Helper App Window")
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        if let helperAppWindowController = storyboard.instantiateController(withIdentifier: "HelperAppWindowController") as? NSWindowController
+        {
+            if let helperAppWindow = helperAppWindowController.window {
+                
+                // 2
+                if let helperAppVC = helperAppWindow.contentViewController as? HelperAppVC {
+                    // 3
+                    let application = NSApplication.shared
+                    application.runModal(for: helperAppWindow)
+                    // 4
+                    helperAppWindow.close()
+                }
+                
+                
+            }
+        }
     }
     
+    // Create the status item
+    @objc func togglePopover(_ sender: AnyObject?) {
+         if let button = self.statusBarItem.button {
+              if self.popover.isShown {
+                   self.popover.performClose(sender)
+              } else {
+                   self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+              }
+         }
+    }
     
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
---
+++ MacAppWithHelperApp/HelperAppVC.swift
@@ -0,0 +1,17 @@
+//
+//  HelperAppVC.swift
+//  MacAppWithHelperApp
+//
+//  Created by Scott Richards on 7/26/22.
+//
+
+import Cocoa
+
+class HelperAppVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
